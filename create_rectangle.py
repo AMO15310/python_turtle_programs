7ca5215f2daf7a035407ad98448831a0fbf6fb34
AMO15310 <wachiraamos402@gmail.com>
2022-06-28 14:18:12 +0300

    create a rectangle without using a loop

---
+++ create_rectangle.py
@@ -0,0 +1,23 @@
+import turtle
+t = turtle.Turtle()
+#select background color
+turtle.bgcolor("blue")
+#select color of the pen
+turtle.pencolor("pink")
+#select the fill color of the pen
+turtle.fillcolor("black")
+#select the title of the screen
+turtle.title("create a rectangle without using a loop")
+#select the speed of the pen
+turtle.speed(1)
+#move the pe forward by 100
+t.fd(100)
+#turn the pen by 90 degrees
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+#end the loop
+turtle.mainloop()
